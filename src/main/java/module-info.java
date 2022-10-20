1ee8ea84c562d8f4accd28f79c74761c6e0beebc
David Bašić <d.basic97.db@gmail.com>
2022-10-20 18:16:36 +0200

    first commit of whole file

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,6 @@ module hr.algebra.java2orlog {
 
     opens hr.algebra.java2orlog to javafx.fxml;
     exports hr.algebra.java2orlog;
+    exports hr.algebra.java2orlog.controllers;
+    opens hr.algebra.java2orlog.controllers to javafx.fxml;
 }

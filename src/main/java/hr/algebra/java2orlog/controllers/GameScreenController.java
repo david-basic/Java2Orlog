852455f462715937277c0469b4d9eb35c0cc5a82
David Bašić <d.basic97.db@gmail.com>
2022-11-09 14:40:25 +0100

    added documentation creation tool

--- src/main/java/hr/algebra/java2orlog/controllers/GameScreenController.java
+++ src/main/java/hr/algebra/java2orlog/controllers/GameScreenController.java
@@ -22,9 +22,18 @@ import javafx.scene.shape.Circle;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.*;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class GameScreenController implements Initializable {
     //region Fields
@@ -38,9 +47,11 @@ public class GameScreenController implements Initializable {
     private int playerTwoCoinCount = 0;
     private int playerOneTotalDamageTaken = 0;
     private int playerTwoTotalDamageTaken = 0;
+    private static final String CLASS_EXTENSION = ".class";
 
     private MoveDetails tempMoveDetails;
     private static List<MoveDetails> playerMoves = new ArrayList<>();
+
     private List<DiceDetails> notChosenDice = new ArrayList<>();
     private List<DiceDetails> playerOneAllDice = new ArrayList<>();
     private List<DiceDetails> playerTwoAllDice = new ArrayList<>();
@@ -1236,14 +1247,179 @@ public class GameScreenController implements Initializable {
     }
 
     @FXML
-    private void SaveGame(){
+    private void SaveGame() {
 
     }
 
     @FXML
-    private void LoadGame(){
+    private void LoadGame() {
 
     }
 
+    @FXML
+    private void createDocumentation() {
+        File documentationFile = new File("documentation.html");
 
+        try {
+
+            FileWriter writer = new FileWriter(documentationFile);
+
+            writer.write("<!DOCTYPE html>");
+            writer.write("<html>");
+            writer.write("<head>");
+            writer.write("<title>Project documentation</title>");
+            writer.write("</head>");
+            writer.write("<body>");
+            writer.write("<h1>Project documentation</h1>");
+            writer.write("<p>Class list:</p>");
+
+            List<Path> paths = Files.walk(Paths.get("."))
+                    .filter(path -> path.getFileName().toString().endsWith(CLASS_EXTENSION))
+                    .collect(Collectors.toList());
+
+            for (Path path : paths) {
+                //System.out.println("Path: " + path);
+                String[] tokens = path.toString().split(Pattern.quote(System.getProperty("file.separator")));
+
+                Boolean startBuildingPath = false;
+
+                StringBuilder sb = new StringBuilder();
+
+                for (String token : tokens) {
+                    if ("classes".equals(token)) {
+                        startBuildingPath = true;
+                        continue;
+                    }
+
+                    if (startBuildingPath) {
+
+                        if (token.endsWith(CLASS_EXTENSION)) {
+                            sb.append(token.substring(0, token.indexOf(".")));
+                        } else {
+                            sb.append(token);
+                            sb.append(".");
+                        }
+                    } else {
+                        continue;
+                    }
+                }
+
+                if ("module-info".equals(sb.toString())) {
+                    continue;
+                }
+
+                System.out.println("Fully qualified name: " + sb.toString());
+
+                try {
+                    Class<?> clazz = Class.forName(sb.toString());
+
+                    writer.write("<h2>" + Modifier.toString(clazz.getModifiers()) + " " + clazz.getName() + "</h2>");
+
+                    StringBuilder classFieldString = new StringBuilder();
+
+                    for (Field classField : clazz.getDeclaredFields()) {
+                        Annotation[] annotations = classField.getAnnotations();
+                        if (annotations.length != 0) {
+                            for (Annotation a : annotations) {
+                                classFieldString.append(a.toString());
+                                classFieldString.append("<br />");
+                            }
+                        }
+                        classFieldString.append(Modifier.toString(classField.getModifiers()));
+                        classFieldString.append(" ");
+                        classFieldString.append(classField.getType().getSimpleName());
+                        classFieldString.append(" ");
+                        classFieldString.append(classField.getName());
+                        classFieldString.append(" ");
+                        classFieldString.append("<br /><br />");
+                    }
+
+                    writer.write("<h3>Fields</h3>");
+
+                    writer.write("<h4>" + classFieldString + "</h4>");
+
+                    Constructor[] constructors = clazz.getConstructors();
+
+                    writer.write("<h3>Constructors:</h3>");
+
+                    for (Constructor c : constructors) {
+
+                        String constructorParams = generateDocumentation(c);
+
+                        writer.write("<h4>Constructor:" + Modifier.toString(c.getModifiers()) + " " + c.getName()
+                                + "(" + constructorParams + ")" + "</h4>");
+                    }
+
+                    Method[] methods = clazz.getMethods();
+
+                    writer.write("<h3>Methods:</h3>");
+
+                    for (Method m : methods) {
+
+                        String methodsParams = generateDocumentation(m);
+
+                        StringBuilder exceptionsBuilder = new StringBuilder();
+
+                        for (int i = 0; i < m.getExceptionTypes().length; i++) {
+                            if (exceptionsBuilder.isEmpty()) {
+                                exceptionsBuilder.append(" throws ");
+                            }
+
+                            Class exceptionClass = m.getExceptionTypes()[i];
+                            exceptionsBuilder.append(exceptionClass.getSimpleName());
+
+                            if (i < m.getExceptionTypes().length - 1) {
+                                exceptionsBuilder.append(", ");
+                            }
+                        }
+
+                        writer.write("<h4>Method:" + Modifier.toString(m.getModifiers())
+                                + " " + m.getReturnType().getSimpleName()
+                                + " " + m.getName() + "(" + methodsParams + ")"
+                                + " " + exceptionsBuilder.toString()
+                                + "</h4>");
+                    }
+
+                } catch (ClassNotFoundException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            writer.write("</body>");
+            writer.write("</html>");
+            writer.close();
+
+        } catch (IOException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error while generating documentation!");
+            alert.setHeaderText("Cannot find the files");
+            alert.setContentText("The class files cannot be accessed.");
+
+            alert.showAndWait();
+        }
+    }
+
+    private <T extends Executable> String generateDocumentation(T executable) {
+        Parameter[] params = executable.getParameters();
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < params.length; i++) {
+            String modifierString = Modifier.toString(params[i].getModifiers());
+
+            if (!modifierString.isEmpty()) {
+                sb.append(modifierString);
+                sb.append(" ");
+            }
+            sb.append(params[i].getType().getSimpleName());
+            sb.append(" ");
+            sb.append(params[i].getName());
+
+            if (i < (params.length - 1)) {
+                sb.append(", ");
+            }
+        }
+
+        return sb.toString();
+    }
 }
